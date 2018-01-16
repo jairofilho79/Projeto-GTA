b29e7f1c592d8264635f1a0ed870044d4c925e8d
jairofilho79 <32720779+jairofilho79@users.noreply.github.com>
2018-01-16 10:47:55 -0200

    Add files via upload

---
+++ introducao/intro.js
@@ -0,0 +1,27 @@
+/*eslint-env jquery*/
+/*jslint browser: true*/
+/*global $, jQuery*/
+
+//evitar que o menu não funcione com os subtópicos.
+var window = window;
+var event  = event;
+$(window).hover(function () {
+    "use strict";
+    var posicao = event.pageY - this.pageYOffset;
+    if (posicao < 50) {
+        $("h1").css("z-index", "-1");
+        $("h2").css("z-index", "-1");
+        $("h3").css("z-index", "-1");
+        $("p").css("z-index", "-1");
+        $("ul").css("z-index", "-1");
+        $(".menu ul").css("z-index", "-0");
+    }
+});
+$("#introducao").hover(function () {
+    "use strict";
+    $("h1").css("z-index", "0");
+    $("h2").css("z-index", "0");
+    $("h3").css("z-index", "0");
+    $("p").css("z-index", "0");
+    $("ul").css("z-index", "0");
+});
